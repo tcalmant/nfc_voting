a1ee7eb7391e82d4d3562eef2d6c10cda80c0d37
Thomas Calmant <thomas.calmant@gmail.com>
2014-05-14 18:13:18 +0200

    Small review of write_nfc.py
    - Avoid using global ID
    - Corrected code format
    - Added header

--- server/write_nfc.py
+++ server/write_nfc.py
@@ -1,19 +1,32 @@
-import nfc
+#!/usr/bin/python
+# -- Content-Encoding: UTF-8 --
+"""
+NFC tag writer: sets up the tag read by the voting machine
+
+:authors: Antoine GERVAIL, Amine ZIAN­CHERIF, Thomas CALMANT
+:license: Eclipse Public License 1.0
+"""
+
+# nfcpy
+import nfc.ndef
+
+# Standard library
+import functools
 import sys
 
-def connected(tag):
-  global id_num
-  sp = nfc.ndef.TextRecord(str(id_num))
-  sp.name = 'id'
-  tag.ndef.message = nfc.ndef.Message(sp)
-  print id_num
-  return True
+def connected(id_num, tag):
+    sp = nfc.ndef.TextRecord(str(id_num))
+    sp.name = 'id'
+    tag.ndef.message = nfc.ndef.Message(sp)
+    print("ID set: {0}".format(id_num))
+    return True
 
-if len(sys.argv) != 2:
-  print 'Usage : write_nfc.py [id]'
-  exit(0)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print "Usage: write_nfc.py [id]"
+        sys.exit(0)
 
-id_num = int(sys.argv[1])
-clf = nfc.ContactlessFrontend('usb')
-tag = clf.connect(rdwr={'on-connect': connected})
-
+    id_num = int(sys.argv[1])
+    clf = nfc.ContactlessFrontend('usb')
+    tag = clf.connect(rdwr={'on-connect':
+                            functools.partial(connected, id_num)})
