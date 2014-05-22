2104fd4ad225df9ec516280796f7a2f0d16ca637
Thomas Calmant <thomas.calmant@gmail.com>
2014-05-22 16:06:22 +0200

    Display script rewritten
    - Removed MongoDB (to ease debugging)
    - Using default MQTT payload format

--- server/affichage.py
+++ server/affichage.py
@@ -2,7 +2,6 @@ import sys, pygame
 import math
 import paho.mqtt.client as mosquitto
 from time import sleep
-import requests
 import json
 '''
 Little display of a mongodb database.
@@ -10,15 +9,13 @@ First fetching the content of the database
 Then each time the database is modified displaying immediatly the changes
 '''
 
+mqtt_host = 'localhost'
 
-def get_score(bd, table):
-  r = requests.get('http://localhost:28017/' + bd + '/' + table + '/')
-  res = json.loads(r.text)
-  score = [0,0,0]
-  for row in res['rows']:
-    score[row['vote']] += 1
-  return score
+# Vote configuration
+scores = {-1: 0, 0: 0, 1: 0}
+names = {1: 'Pour', -1: 'Contre', 0: 'Neutre'}
 
+# Initialize PyGame screen
 pygame.init()
 size = width, height = 600, 600
 screen = pygame.display.set_mode(size)
@@ -27,38 +24,52 @@ h_bas = height - 10
 myfont = pygame.font.SysFont("Comic Sans MS", 80)
 pink = (255, 0, 255)
 blue = (0, 255, 255)
-score = get_score('nfcDB', 'vote')
-name = ['Pour', 'Contre', 'Neutre']
 
 #MQTT part
-def on_message(mosq, msg):
-  global score
-	print("one more for " + str(msg.payload) + str(score))
-	score[int(msg.payload)] += 1
-mqttc = mosquitto.Mosquitto("bla")
+def on_message(client, data, msg):
+    timestamp, uid, value = str(msg.payload).split(',')
+    value = int(value)
+    print "One more for", names[value]
+    scores[value] += 1
+
+mqttc = mosquitto.Mosquitto("nfc-vote-visu")
 mqttc.on_message = on_message
-mqttc.connect("127.0.0.1", 1883, 10000)
-mqttc.subscribe("success", 0)
+mqttc.connect(mqtt_host, 1883)
+mqttc.subscribe("nfc-vote", 0)
 
 while 1:
-	sleep(0.5)
-	mqttc.loop()
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
-	screen.fill((0,0,0))
-	max_score = max(score)
-	if max_score != 0:
-		rapport = 500 / max_score
-	for i, val in enumerate(score):
-		xx = 200 * i
-		label = myfont.render(str(val), 1, pink)
-		title = myfont.render(name[i], 1, blue)
-		screen.blit(pygame.transform.rotozoom(title,0,0.5), (xx + 60, 50))
-		if val == 0:
-			screen.blit(pygame.transform.rotozoom(label,45,0.5), (xx + 82, 550))
-		else:
-			hauteur = val * rapport
-			rect = pygame.Rect(xx + 20, h_bas - hauteur, 160, hauteur)
-			pygame.draw.rect(screen, (50,50,50), rect)
-			screen.blit(pygame.transform.rotozoom(label,45,0.5), (xx + 82 - math.log(val,10)*7, min(550,h_bas - hauteur + 20)))
-	pygame.display.update()
+    sleep(0.2)
+    mqttc.loop()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            break
+            
+    screen.fill((0,0,0))
+    
+    max_score = max(scores.values())
+    if max_score != 0:
+        rapport = 500 / max_score
+    else:
+        rapport = 1
+        
+    for i, value in enumerate(scores.keys()):
+        xx = 200 * i
+        votes = scores[value]
+        label = myfont.render(str(votes), 1, pink)
+        title = myfont.render(names[value], 1, blue)
+        
+        screen.blit(pygame.transform.rotozoom(title,0,0.5), (xx + 60, 50))
+        
+        if votes == 0:
+            screen.blit(pygame.transform.rotozoom(label,45,0.5), (xx + 82, 550))
+            
+        else:
+            hauteur = votes * rapport
+            
+            rect = pygame.Rect(xx + 20, h_bas - hauteur, 160, hauteur)
+            pygame.draw.rect(screen, (50,50,50), rect)
+            screen.blit(pygame.transform.rotozoom(label,45,0.5),
+                        (xx + 82 - math.log(votes,10)*7,
+                         min(550,h_bas - hauteur + 20)))
+    
+    pygame.display.update()
